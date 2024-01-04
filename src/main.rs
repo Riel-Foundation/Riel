e0ee6833d833079fc0efa2402c62f4dfbc02aa81
Sellsword9 <yerayromeroortiz@gmail.com>
2024-01-05 00:48:55 +0100

    Structurization

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,18 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-
-    
-    if args.len() > 1 {
-        if args[1] == "--help" {
-            println!("usage: riel <command> [<args>]");
-        } else {
-            println!("Welcome to Riel! Try using the --help flag for more information.");
+    let clean_args: Vec<String> = args.iter().map(|x| x.to_lowercase()).collect();
+    let fixed_args: Vec<String> = clean_args.iter().map(|x| x.replace(" ", "")).collect();
+    match fixed_args.len() { // NOTE: This structure is bound to change
+        0 => println!
+        ("Welcome to Riel! Try help or --help for more information, or init / create to start a repository."),
+        1 => match args[0].as_str() {
+            "help" => println!("Welcome to Riel! Try help or --help for more information, or init / create to start a repository."),
+            "--help" => println!("Welcome to Riel! Try help or --help for more information, or init / create to start a repository."),
+            "init" => println!("Initializing repository..."), //TODO
+            "create" => println!("Creating repository..."), // TODO
+            _ => println!("Welcome to Riel! Try help or --help for more information, or init / create to start a repository."),
+        },
+        _ => println!("Welcome to Riel! Try help or --help for more information, or init / create to start a repository."),
         }
-    } else {
-        println!("Welcome to Riel! Try using the --help flag for more information.");
     }
-}
