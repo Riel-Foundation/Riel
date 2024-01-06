15842537b8e1393c2ca508e89ee0d6e80784ead4
Yeray Romero <yerayromerodev@gmail.com>
2024-01-06 22:36:05 +0100

    Testing folder creation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -9,8 +10,8 @@ fn main() {
         1 => match fixed_args[0].as_str() {
             "help" => println!("Welcome to Riel! Try help or --help for more information, or init / create to start a repository."),
             "--help" => println!("Welcome to Riel! Try help or --help for more information, or init / create to start a repository."),
-            "init" => println!("Initializing repository..."), //TODO
-            "create" => println!("Creating repository..."), // TODO
+            "init" => mount_repo(),
+            "mount" => mount_repo(),
             _ => println!("Welcome to Riel! Try help or --help for more information, or init / create to start a repository."),
         },
         _ => println!("Welcome to Riel! Try help or --help for more information, or init / create to start a repository."),
@@ -19,4 +20,10 @@ fn main() {
 fn fix_args(args: Vec<String>) -> Vec<String> {
     let clean_args: Vec<String> = args.iter().map(|x| x.to_lowercase()).collect();
     clean_args.iter().map(|x| x.replace(" ", "")).collect()
+}
+fn mount_repo() -> () {
+   // Create a .riel folder
+   fs::create_dir(".riel").expect("Failed to create .riel folder, please check your permissions.");
+    // Create a .riel/objects folder
+    fs::create_dir(".riel/commits").expect("Failed to create .riel/objects folder, please check your permissions.");
 }
