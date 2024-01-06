4fe177b5c1b768df8615bca7dbbc7105c52c5b40
Yeray Romero <yerayromerodev@gmail.com>
2024-01-06 22:30:31 +0100

    Small correction and refactoring

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,11 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let clean_args: Vec<String> = args.iter().map(|x| x.to_lowercase()).collect();
-    let fixed_args: Vec<String> = clean_args.iter().map(|x| x.replace(" ", "")).collect();
+    let fixed_args = fix_args(args);
     match fixed_args.len() { // NOTE: This structure is bound to change
         0 => println!
         ("Welcome to Riel! Try help or --help for more information, or init / create to start a repository."),
-        1 => match args[0].as_str() {
+        1 => match fixed_args[0].as_str() {
             "help" => println!("Welcome to Riel! Try help or --help for more information, or init / create to start a repository."),
             "--help" => println!("Welcome to Riel! Try help or --help for more information, or init / create to start a repository."),
             "init" => println!("Initializing repository..."), //TODO
@@ -17,3 +16,7 @@ fn main() {
         _ => println!("Welcome to Riel! Try help or --help for more information, or init / create to start a repository."),
         }
     }
+fn fix_args(args: Vec<String>) -> Vec<String> {
+    let clean_args: Vec<String> = args.iter().map(|x| x.to_lowercase()).collect();
+    clean_args.iter().map(|x| x.replace(" ", "")).collect()
+}
