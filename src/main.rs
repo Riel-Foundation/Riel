5858e7db59c21916dcf7c56ce196b022f34d5bba
Yeray Romero <yerayromerodev@gmail.com>
2024-01-07 14:47:06 +0100

    Tiny refactoring

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,16 @@ use std::env;
 use std::fs;
 
 fn main() {
+    const RIEL_WORKS: &str = "Riel works! Try help or --help for more information";
+    const HELP: &str = "Welcome to Riel! Try help or --help for more information, or init / create to start a repository.";
     let args: Vec<String> = env::args().collect();
     let fixed_args = fix_args(args);
     match fixed_args.len() { // NOTE: This structure is bound to change
         1 => println!
-        ("Riel works! Try help or --help for more information"),
+        ("{}", RIEL_WORKS),
         2 => match fixed_args[1].as_str() {
-            "help" => println!("Welcome to Riel! Try help or --help for more information, or init / create to start a repository."),
-            "--help" => println!("Welcome to Riel! Try help or --help for more information, or init / create to start a repository."),
+            "help" => println!("{}", HELP),
+            "--help" => println!("{}", HELP),
             "init" => mount_repo(),
             "mount" => mount_repo(),
             _ => println!("{} is not a valid command. Try help or --help for more information.", fixed_args[0]),
@@ -28,7 +30,7 @@ fn mount_repo() -> () {
         create_repo();
         println!("Riel repository created successfully.");
     }
-    
+
     fn create_repo() -> () {
         fs::create_dir(".riel").expect("Failed to create .riel directory, please check your permissions.");
         fs::create_dir(".riel/commits").expect("Failed to create .riel/objects directory, but .riel worked. Please check your storage & folder structure.");
