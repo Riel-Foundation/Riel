a001ee43476708ac7c990b720e2e34da7c0f28dd
Yeray Romero <yerayromerodev@gmail.com>
2024-01-14 22:05:25 +0100

    Advancing the file merging

--- src/filemerger.rs
+++ src/filemerger.rs
@@ -1,6 +1,23 @@
+use std::fs::File;
+use crate::CommitMetadata;
+use std::io::{BufRead, BufReader};
+
 // Module: filemerger
-
-
-pub fn testing() {
-  println!("Hello World");
+pub fn merge_files(before: &File, after: &File) -> Option<CommitMetadata> {
+  let before_lines: Vec<String> = BufReader::new(before)
+    .lines()
+    .map(|line| line.unwrap())
+    .collect();
+  let after_lines: Vec<String> = BufReader::new(after)
+    .lines()
+    .map(|line| line.unwrap())
+    .collect();
+  let mut diff_lines: Vec<String> = Vec::new();
+  for l in after_lines {
+    if !(l == "") && !before_lines.contains(&l) {
+      diff_lines.push(l);
+    }
+  }
+  println!("{:?}", diff_lines);
+  todo!()
 }
