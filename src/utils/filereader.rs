1e60576ba3e80bc90b95269764ff2cfa882f8408
Yeray Romero <yerayromerodev@gmail.com>
2024-01-20 18:31:19 +0100

    Added a file reader module for easier management

---
+++ src/utils/filereader.rs
@@ -0,0 +1,13 @@
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
+pub fn open_path_and_read_to_string(path: &str) -> String {
+  let p: &Path = Path::new(path);
+  let mut file: File = File::open(p)
+    .expect(&format!("Filereader.rs --> Could not open file while using path: {}", path));
+  let mut file_str: String = String::new();
+  file.read_to_string(&mut file_str)
+    .expect(&format!("Filereader.rs --> Could not read file while using path: {}", path));
+  
+  file_str
+}
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,2 +1,4 @@
 pub mod datetime;
+pub mod filereader;
+use filereader::open_path_and_read_to_string;
 use datetime::DateTime;
