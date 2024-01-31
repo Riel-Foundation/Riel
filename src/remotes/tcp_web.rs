a258b122529413d7ee4839801f5b53e3b5518aa3
Yeray Romero <yerayromerodev@gmail.com>
2024-01-31 19:51:35 +0100

    0.2.1, fixed 0.2.0 bug on file content

--- src/remotes/tcp_web.rs
+++ src/remotes/tcp_web.rs
@@ -2,9 +2,12 @@ use serde::{Deserialize, Serialize};
 use serde_json::{json, Result as JsonResult, Value};
 use std::collections::{HashMap, VecDeque};
 use std::error::Error;
+use std::ffi::{OsStr, OsString};
 use std::fs;
-use std::io::{Read, Write};
+use std::io::{self, BufRead, BufReader, Read, Write};
 use std::net::TcpStream;
+use std::os::unix::ffi::OsStrExt;
+use std::os::unix::fs::PermissionsExt;
 use std::ptr::null;
 
 #[derive(Debug, Deserialize)]
@@ -31,26 +34,12 @@ pub fn web_get_with_url(url: &str) -> Option<TcpStream> {
     stream
         .write_all(request.as_bytes())
         .expect("Failed to write to stream.");
-    let mut buffer: String = String::new();
+
     Some(stream)
 }
 fn read_http_response(stream: &mut TcpStream) -> Result<String, std::io::Error> {
-    let mut buffer = [0; 1024];
-    let mut response = String::new();
-
-    loop {
-        let bytes_read = stream.read(&mut buffer)?;
-        if bytes_read == 0 {
-            break;
-        }
-
-        response.push_str(String::from_utf8_lossy(&buffer[..bytes_read]).as_ref());
-
-        if response.contains("\r\n\r\n") {
-            break;
-        }
-    }
-
+    let mut response: String = String::new();
+    stream.read_to_string(&mut response)?;
     Ok(response)
 }
 pub fn receive_directory_structure(stream: &mut TcpStream, base_path: &str) -> bool {
@@ -66,15 +55,12 @@ pub fn receive_directory_structure(stream: &mut TcpStream, base_path: &str) -> b
 fn clean_response(response: String) -> String {
     let mut full_lines = response.lines().collect::<Vec<&str>>();
     let mut clean_lines: Vec<&str> = Vec::new();
-    //println!("Full lines: {:?}", full_lines);
     for line_index in 0..full_lines.len() {
         if full_lines[line_index].trim().is_empty() {
-            //println!("Found empty line at index {}", line_index);
             clean_lines = full_lines[line_index + 1..].to_vec();
             break;
         }
     }
-    //println!("Clean lines: {:?}", clean_lines);
     clean_lines.join("\n")
 }
 fn create_structure(from: String, here: &str) -> Option<StructureAbstraction> {
@@ -96,20 +82,87 @@ fn structure_process(mut q: &mut VecDeque<StructureAbstraction>, path: &str) {
     if let Some(structure) = q.pop_front() {
         for child in structure.children {
             let child_path = format!("{}/{}", path, child.name);
-            if let Some(url) = child.url { //this child's a file
+            if let Some(url) = child.url {
+                //this child's a file
                 let mut stream = web_get_with_url(&url);
                 if let Some(mut s) = stream {
-                    let file_content = read_http_response(&mut s)
-                        .expect(
-                            &format!("The reader should have worked at {}", url));
-                    fs::write(&child_path, file_content)
-                    .expect(&format!("Failed to write file at {}.", child_path));
+                    let file_content = process_file(&mut s, &url);
+                    match file_content.failed {
+                        true => {
+                            println!("Failed to process file.");
+                            continue;
+                        }
+                        false => match file_content.is_utf8 {
+                            true => {
+                                fs::write(&child_path, file_content.utf8)
+                                    .expect("Failed to write file.");
+                            }
+                            false => {
+                                fs::write(&child_path, file_content.binary)
+                                    .expect("Failed to write file.");
+                                // we now remove the header from the http
+                                if try_clean_file(&child_path) {
+                                    println!("Cleaned file. Binary file was cloned successfully.");
+                                } else {
+                                    println!("Failed to clean binary file.");
+                                }
+                            }
+                        },
+                    }
                 }
-            } else { //this child's a directory
+            } else {
+                //this child's a directory
                 fs::create_dir_all(&child_path).expect("Failed to create directory.");
                 q.push_back(child);
                 structure_process(q, &child_path);
             }
         }
     }
+}
+fn process_file(stream: &mut TcpStream, tcp_url: &str) -> FileContent {
+    let mut buffer: String = String::new();
+    let mut stream_clone: TcpStream = stream.try_clone().unwrap();
+    let response: Result<usize, io::Error> = stream.read_to_string(&mut buffer);
+    if response.is_err() && response.err().unwrap().kind() == std::io::ErrorKind::InvalidData {
+        println!("Invalid data received. Non-utf8 files are not supported yet.");
+        println!("Trying to read as binary...");
+        return process_binary_file(&mut web_get_with_url(tcp_url).unwrap());
+    }
+    buffer = clean_response(buffer);
+    FileContent {
+        is_utf8: true,
+        failed: false,
+        utf8: buffer,
+        binary: Vec::new(),
+    }
+}
+fn process_binary_file(stream: &mut TcpStream) -> FileContent {
+    let mut buffer: Vec<u8> = Vec::new();
+    let response = stream.read_to_end(&mut buffer);
+    if response.is_err() {
+        println!("Failed to read binary file.");
+        return FileContent {
+            is_utf8: false,
+            failed: true,
+            utf8: String::new(),
+            binary: Vec::new(),
+        };
+    }
+    let binary: Vec<u8> = buffer;
+    FileContent {
+        is_utf8: false,
+        failed: false,
+        utf8: String::new(),
+        binary,
+    }
+}
+fn try_clean_file(path: &str) -> bool {
+  //TODO
+    false
+}
+struct FileContent {
+    is_utf8: bool,
+    utf8: String,
+    binary: Vec<u8>,
+    failed: bool,
 }
